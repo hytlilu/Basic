883b4fdac2929aad8eb214646d92a4ceed798c70
lilu <hyt_amy@163.com>
2019-05-29 10:17:26 +0800

    拆装箱

--- src/test/AssertTest.java
+++ src/test/AssertTest.java
@@ -45,18 +45,48 @@ public class AssertTest {
 
     @Test
     public void testNumber() {
-        String s1 = "abc";
-        String s2 = "abc";
-        String s3 = new String("abc");
-        String s4 = new String ("abc");
-        String s5 = "a";
+//        String s1 = "abc";
+//        String s2 = "abc";
+//        String s3 = new String("abc");
+//        String s4 = new String ("abc");
+//        String s5 = "a";
+//
+//        System.out.println(s1==s2);
+//        System.out.println(s1==s3);
+//        System.out.println(s3==s4);
+//        System.out.println(s1==("a"+"bc"));
+//        System.out.println(s1==(s5+"bc"));
 
-        System.out.println(s1==s2);
-        System.out.println(s1==s3);
-        System.out.println(s3==s4);
-        System.out.println(s1==("a"+"bc"));
-        System.out.println(s1==(s5+"bc"));
 
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+
+        Long l3 = 3L;
+        
+        long ll3 = 3L;
+
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(l3.equals(a + b));
+        System.out.println(l3 == (a + b));
+        System.out.println(ll3 == l3);
+        System.out.println(l3.equals(ll3));
+        System.out.println(ll3 == (a+b));
+
+    }
+
+
+    @Test
+    public void testBoolean(){
+        boolean a = true;
+        boolean b = false;
+        boolean[] list = new boolean[2];
     }
 
 }
